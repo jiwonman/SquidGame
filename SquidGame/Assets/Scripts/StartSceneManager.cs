bb01ffd7821f8c7cc7de3e9a03ab6a0b84adced3
jiwonman <kusa1230@naver.com>
2022-06-01 04:23:48 +0900

    Start Scene 완성

--- SquidGame/Assets/Scripts/StartSceneManager.cs
+++ SquidGame/Assets/Scripts/StartSceneManager.cs
@@ -2,11 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartSceneManager : MonoBehaviour
 {
-    // 메인 씬을 불러오는 메소드
-    public void MovetoNextScene()
+    public Image original;
+
+    public GameObject Canvas_First;
+    public GameObject Canvas_Second;
+    public GameObject Canvas_Third;
+    public GameObject Canvas_Fourth;
+    public GameObject Canvas_Fifth;
+
+    // Canvas 변경 
+    public void ChangeCanvas(int i)
+    {
+        if(i == 0) // Go to Second Canvas
+        {
+            Canvas_First.SetActive(false);
+            Canvas_Second.SetActive(true);
+            Canvas_Third.SetActive(false);
+            Canvas_Fourth.SetActive(false);
+            Canvas_Fifth.SetActive(false);
+        }
+        else if (i == 1) // Go to First Canvas
+        {
+            Canvas_First.SetActive(true);
+            Canvas_Second.SetActive(false);
+            Canvas_Third.SetActive(false);
+            Canvas_Fourth.SetActive(false);
+            Canvas_Fifth.SetActive(false);
+        }
+        else if (i == 2) // Go to Third Canvas
+        {
+            Canvas_First.SetActive(false);
+            Canvas_Second.SetActive(false);
+            Canvas_Third.SetActive(true);
+            Canvas_Fourth.SetActive(false);
+            Canvas_Fifth.SetActive(false);
+        }
+        else if (i == 3) // Go to Fourth Canvas
+        {
+            Canvas_First.SetActive(false);
+            Canvas_Second.SetActive(false);
+            Canvas_Third.SetActive(false);
+            Canvas_Fourth.SetActive(true);
+            Canvas_Fifth.SetActive(false);
+        }
+        else //Go to Fifth Canvas
+        {
+            Canvas_First.SetActive(false);
+            Canvas_Second.SetActive(false);
+            Canvas_Third.SetActive(false);
+            Canvas_Fourth.SetActive(false);
+            Canvas_Fifth.SetActive(true);
+        }
+    }
+
+    public void MovetoMainScene()
     {
         SceneManager.LoadScene("MainScene");
     }
